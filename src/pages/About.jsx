2a98ecb78460a319dc67d19a8ff9e1b3732eed6c
Ankit Chaudhary <Ankitchy158@gmail.com>
2026-08-10 22:09:35 +0545

    setup react

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>About
+
+        <a href="/">Landing</a>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div>Landing
+
+        <a href="/about">About</a>
+      
+    </div>
+  )
+}
+
+export default Landing

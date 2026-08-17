478372cc5919b060b2f5ad226dc9b019764b4fdf
Ankit Chaudhary <Ankitchy158@gmail.com>
2026-08-17 22:30:59 +0545

    created FamousTrip section

--- src/components/common/landingComponent/FamousTrips.jsx
+++ src/components/common/landingComponent/FamousTrips.jsx
@@ -1,11 +1,70 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const FamousTrips = () => {
+const tripsData =[
+  {
+    title: "Bhatbhateni",
+    content: "Our Shopping center opens 24/7, so you can access it anytime.",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcFtrUDldo2EfKRCl7bOIr4IcXW5htCluDZ1r0viGkow&s=10"
+  },
+
+  {
+    title: "Arnapurna Base Camp",
+    content: "Annapurna Base Camp (ABC) sits in a high glacial basin at an elevation of 4,130 meters (13,550 feet) in central Nepal.",
+    image: "https://lh3.googleusercontent.com/grass-cs/ACvplmNrCvoAdZ1teae-8ZKXz8ii186n4FNYN45TD7ijIS1qOf7GBSckDTwzxn_Fqh2gxsusW2oScQBbpCOdN6QXLo0jVYva515ML001MhPf2WhWEyv5lcEbuuo82l3qxDuGNyTzvdKrI4zIZ0xW=w326-h312-n-k-no",
+    
+  },
+
+  {
+    title: "Invite Collaborators",
+    content: "Our website allows you to invite collaborators to work on your projects together.",
+    image: "https://travellingnepaltips.com/wp-content/uploads/2018/02/26815069_1594937403932618_5439460490865958001_n.jpg",
+  },
+
+  {
+    title: "Itenirary",
+    content: "Our website allows you to create and manage your itinerary, so you can plan your trips easily.",
+    image : "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Biratnagar_Jaljala_Chowk.jpg/250px-Biratnagar_Jaljala_Chowk.jpg?utm_source=en.wikipedia.org&utm_campaign=parser&utm_content=thumbnail"
+  }
+
+]
+
+const FamousTrips = ({link}) => {
+
+  const navigate = useNavigate();
   return (
-    <div>
-      
+  
+    <div className= " px-20 py-24 ">
+        {/* heading */}
+
+        <div>
+            <h2 className="text-4xl font-bold 
+            text-center ">Famous Trips</h2>
+
+        </div>
+        
+        {/* content */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-20 ">
+         {
+            tripsData.map((feature,index)=>{
+              return(
+                 <div onClick={()=>{navigate(feature.link)}} className="border rounded p-4 border-gray-300 bg-red-50 md:bg-amber-400 lg:bg-fuchsia-400">
+
+                  <div className='w-full h-40 overflow-hidden'>
+                    <img className="w-full" src={feature.image} alt={feature.title} />
+                  </div>
+
+                    <h3 className='text-xl font-bold mb-4'>{feature.title}</h3>
+                    <p>{feature.content}</p>
+                    
+                 </div>
+              )
+            })
+         }
+         </div>
     </div>
   )
 }
 
 export default FamousTrips
+
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/common/Navbar'
 import Hero from '../components/common/landingComponent/Hero'
 import Features from '../components/common/landingComponent/Features'
+import FamousTrip from '../components/common/landingComponent/FamousTrips'
 
 const Landing = () => {
   return (
@@ -9,6 +10,7 @@ const Landing = () => {
       <Navbar />
       <Hero />
       <Features />
+      <FamousTrip />
     </div>
   )
 }

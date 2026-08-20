4a121d2c73f7b0f2dc23867006043a4c35c9cfae
Ankit Chaudhary <Ankitchy158@gmail.com>
2026-08-20 23:35:47 +0545

    added shadcn in my project and used shadcn components

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import Navbar from '../components/common/Navbar'
+import { Button } from '../components/ui/button'
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
+
+
 
 const Contact = () => {
   return (
     <div>
         <Navbar />
+
+         <Button>Button</Button>
+         <Button variant='destructive'>clickme</Button>
+
+         <HoverCard>
+  <HoverCardTrigger>Hover</HoverCardTrigger>
+  <HoverCardContent>
+    The React Framework – created and maintained by @vercel.
+    <img src='/heroImage.jpg' alt='hero'/>
+  </HoverCardContent>
+</HoverCard>
+         
+
       
     </div>
   )
